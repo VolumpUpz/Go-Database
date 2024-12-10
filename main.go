0e72dbe7b99e5e1d0f6f7bff26a91a0b1eb3dd48
VolumpUpz <132444587+VolumpUpz@users.noreply.github.com>
2024-12-10 20:59:43 +0700

    CR database

--- main.go
+++ main.go
@@ -44,16 +44,22 @@ func main() {
 
 	fmt.Println("Connection Database Successful")
 
-	err = createProduct(&Product{
-		Name:  "Go Product",
-		Price: 222,
-	})
+	// err = createProduct(&Product{
+	// 	Name:  "Go Product",
+	// 	Price: 222,
+	// })
 
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println("Create product successful")
+
+	product, err := getProduct(1)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Create product successful")
+	fmt.Println(product)
 
 }
 
@@ -61,4 +67,16 @@ func createProduct(product *Product) error {
 
 	_, err := db.Exec("INSERT INTO PRODUCTS (name,price) VALUES($1,$2);", product.Name, product.Price)
 	return err
+}
+
+func getProduct(id int) (Product, error) {
+	var p Product
+	row := db.QueryRow("SELECT id,name,price FROM PRODUCTS WHERE id = $1;", id)
+
+	err := row.Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return Product{}, err
+	}
+	return p, nil
+
 }
